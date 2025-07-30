fcc663837f1b3a4e447a703a95fe9bd3ceb90737
uvu1 <53385458+uvu1@users.noreply.github.com>
2025-07-31 03:23:21 +0900

    macbookでのフォントサイズを変更

--- dot_config/wezterm/executable_wezterm.lua
+++ dot_config/wezterm/executable_wezterm.lua
@@ -13,8 +13,10 @@ appearence.apply_config(config)
 config.mux_enable_ssh_agent = false
 
 local os_name = wezterm.target_triple
-
-if string.find(os_name, "darwin") or string.find(os_name, "linux") then
+if string.find(os_name, "darwin") then
+    config.default_prog = { "/bin/zsh" }
+    config.font_size = 14
+elseif string.find(os_name, "linux") then
     config.default_prog = { "/usr/bin/zsh" }
 elseif string.find(os_name, "windows") then
     config.default_prog = { "C:\\Program Files\\PowerShell\\7-preview\\pwsh.exe" }
