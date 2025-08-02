8c0695732af328d6aa5e3a2a2c3228926bc1c80e
uvu1 <53385458+uvu1@users.noreply.github.com>
2025-08-02 16:43:53 +0900

    nvim clipboard

--- dot_config/nvim/lua/option.lua
+++ dot_config/nvim/lua/option.lua
@@ -1,5 +1,4 @@
 vim.opt.number = true
-
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -20,3 +19,27 @@ vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.clipboard = "unnamedplus"
+
+-- if vim.fn.has("wsl") == 1 then
+--     if vim.fn.executable("wl-copy") == 0 then
+--         print("wl-clipboard not found, clipboard integration won't work")
+--     else
+--         vim.g.clipboard = {
+--             name = "wl-clipboard (wsl)",
+--             copy = {
+--                 ["+"] = 'wl-copy --foreground --type text/plain',
+--                 ["*"] = 'wl-copy --foreground --primary --type text/plain',
+--             },
+--             paste = {
+--                 ["+"] = (function()
+--                     return vim.fn.systemlist('wl-paste --no-newline | sed -e "s/\r$//"', {''}, 1) -- '1' keeps empty lines
+--                 end),
+--                 ["*"] = (function() 
+--                     return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', {''}, 1)
+--                 end),
+--             },
+--             cache_enabled = true
+--         }
+--     end
+-- end
