fbe71d885f46b4ca5583edb981e39ac011fc211a
uvu1 <53385458+uvu1@users.noreply.github.com>
2025-08-05 17:29:52 +0900

    relay windows-side pipe of discord into unix socket when start nvim

--- dot_config/sheldon/sync/aliases.zsh
+++ dot_config/sheldon/sync/aliases.zsh
@@ -15,3 +15,16 @@ alias find="fd --color=always --hidden --exclude .git"
 alias grep="rg --color=always --line-number --smart-case"
 alias rg="rg --color=always --line-number --smart-case"
 
+nvim() {
+    if ! pidof socat > /dev/null 2>&1; then
+        [ -e /tmp/discord-ipc-0 ] && rm -f /tmp/discord-ipc-0
+        socat UNIX-LISTEN:/tmp/discord-ipc-0,fork \
+            EXEC:"npiperelay.exe //./pipe/discord-ipc-0" 2>/dev/null &
+    fi
+
+    if [ $# -eq 0 ]; then
+        command nvim
+    else
+        command nvim "$@"
+    fi
+}
