b7c52b3b605e401f1b0006c3b3791474d4c27d4f
uvu1 <53385458+uvu1@users.noreply.github.com>
2025-08-16 14:48:38 +0900

    change nvim config

---
+++ dot_config/nvim/lua/plugins/kubernetes.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "diogo464/kubernetes.nvim",
+        opts = {
+            schema_strict = true,
+            schema_generate_always = true,
+            patch = true,
+        }
+    }
+}
