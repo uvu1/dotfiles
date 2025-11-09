11ac31b6f641b72442667e490cc6239ade5e666d
uvu1 <53385458+uvu1@users.noreply.github.com>
2025-11-09 15:10:24 +0900

    show wezterm titlebar

--- dot_config/wezterm/executable_appearence.lua
+++ dot_config/wezterm/executable_appearence.lua
@@ -7,13 +7,12 @@ function module.apply_config(config)
         horizontal = "Center",
         vertical = "Center",
     }
-    config.window_decorations = "RESIZE"
     config.use_fancy_tab_bar = false
     config.show_new_tab_button_in_tab_bar = false
-    config.window_frame = {
-        inactive_titlebar_bg = "none",
-        active_titlebar_bg = "none",
-    }
+    -- config.window_frame = {
+    --     inactive_titlebar_bg = "none",
+    --     active_titlebar_bg = "none",
+    -- }
     config.win32_system_backdrop = "Acrylic"
     config.macos_window_background_blur = 20
 end
