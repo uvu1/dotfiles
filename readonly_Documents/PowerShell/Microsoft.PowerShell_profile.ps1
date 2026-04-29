4524851db23d0632f911ce6a1303afa691572c61
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-04-30 05:07:42 +0900

    Change powershell

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+$profileDir = Join-Path $global:ProfileRoot "profile.d"
 
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
-Invoke-Expression (& 'C:\Program Files\starship\bin\starship.exe' init powershell --print-full-init | Out-String)
-
-function wsl {
-    wsl.exe --cd /home/uvu @args
+if (Test-Path $profileDir) {
+    Get-ChildItem -Path $profileDir -Filter "*.ps1" |
+        Sort-Object Name |
+        ForEach-Object {
+            . $_.FullName
+        }
 }
