3e211a2ab92d7ab11cb731b8f3d6ee06372a7ec5
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-06-03 19:58:51 +0900

    fix: copy/paste nvim on wsl

--- dot_config/nvim/lua/config/option.lua
+++ dot_config/nvim/lua/config/option.lua
@@ -23,3 +23,27 @@ vim.opt.smartcase = true
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+-- for WSL 
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("wl-copy") == 0 then
+    print("wl-copy is not installed, clipboard integration will not work")
+  else
+    vim.g.clipboard = {
+      name = "wl-clipboard(wsl)",
+      copy = {
+        ["+"] = "wl-copy --foreground --type text/plain",
+        ["*"] = "wl-copy --foreground --primary --type text/plain",
+      },
+      paste = {
+        ["+"] =(function()
+          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', {''}, 1)
+        end),
+        ["*"] = (function ()
+          return vim.fn.systemlist('wl-paste --no-newline --primary|sed -e "s/\r$//"', {''}, 1)
+        end),
+      },
+       cache_enabled = true,
+    }
+  end
+end
