45879ee2d688670fc8b535f46e36f18eb95c44ab
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-07-27 15:23:41 +0900

    connect to mux server

--- mise/dotfiles/.config/wezterm/wezterm.lua
+++ mise/dotfiles/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local appearance = require("appearance")
 local keybinds = require("keybinds")
+local statusbar = require("statusbar")
 
 local config = wezterm.config_builder()
 
@@ -21,7 +22,19 @@ elseif string.find(osName, "linux") then
   config.default_prog = { "/usr/bin/zsh", "-l" }
 end
 
+-- mux server 上でタブ/ペインを保持し、GUI の再起動やクラッシュでセッションを失わない。
+-- Windows は GUI の起動方法を変えたくないため対象外。
+-- GUI と mux server のバージョンが食い違って接続できなくなった場合は
+-- `wezterm cli kill-server` でサーバを落としてから起動し直す。
+if not string.find(osName, "windows") then
+  config.unix_domains = {
+    { name = "unix" },
+  }
+  config.default_gui_startup_args = { "connect", "unix" }
+end
+
 appearance.apply(config)
 keybinds.apply(config)
+statusbar.apply(config)
 
 return config
