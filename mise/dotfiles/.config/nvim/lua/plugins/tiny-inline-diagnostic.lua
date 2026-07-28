530fc83c9bbd0060c3593a3d6f7d1234a62b44c4
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-07-28 16:28:17 +0900

    nvim: LSP/補完を移行（mason/lspconfig/blink/tiny-inline-diagnostic・sidekick死にコード除去）

---
+++ mise/dotfiles/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "LspAttach",
+    priority = 1000,
+    config = function()
+      vim.diagnostic.config({ virtual_text = false, virtual_lines = false })
+      require("tiny-inline-diagnostic").setup({
+        preset = "modern",
+        options = {
+          multilines = true,
+        },
+      })
+    end,
+  },
+}
