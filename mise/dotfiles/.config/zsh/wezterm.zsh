04f32ce86998b2a49a07ad13ef5b84e02e791e74
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-08-06 20:17:17 +0900

    wezterm: WSLペインのcwdをOSC 7で通知するように

--- mise/dotfiles/.config/zsh/wezterm.zsh
+++ mise/dotfiles/.config/zsh/wezterm.zsh
@@ -26,6 +26,32 @@ else
   __wezterm_set_user_var WEZTERM_SHELL zsh
 fi
 
+# pane を分割したときに cwd を引き継ぐには wezterm 側が cwd を知っている必要がある。
+# WSL ペインの実体は mux server (Windows プロセス) が spawn した wsl.exe なので、
+# wezterm のプロセス走査では Windows 側の cwd しか取れない。OSC 7 で Linux のパスを
+# 直接教える。ただし Windows の wezterm はそのパスへ chdir できないため、実際の
+# 引き継ぎは keybinds.lua の spawn_in_current_cwd が wsl.exe --cd で行う。対で扱うこと。
+# UNC パス (wslpath -m) を載せる案は wezterm が URL の host を捨てるため使えない。
+__wezterm_osc7() {
+  # zsh の path は PATH に tie された特殊パラメータなので local 名に使わない。
+  # LC_ALL=C でバイト単位に落とし、マルチバイトも UTF-8 のまま percent-encode する。
+  local LC_ALL=C
+  local dir=$PWD encoded= i c
+
+  for (( i = 1; i <= $#dir; i++ )); do
+    c=$dir[i]
+
+    if [[ $c == [A-Za-z0-9/._~-] ]]; then
+      encoded+=$c
+    else
+      printf -v c '%%%02X' "'$c"
+      encoded+=$c
+    fi
+  done
+
+  printf '\033]7;file://%s%s\033\\' "$HOST" "$encoded"
+}
+
 # precmd はプロンプトのたびに走る。既に空を送っていれば base64 を fork しない。
 typeset -g __wezterm_prog_cleared=1
 
@@ -35,6 +61,8 @@ __wezterm_preexec() {
 }
 
 __wezterm_precmd() {
+  __wezterm_osc7
+
   (( __wezterm_prog_cleared )) && return 0
 
   __wezterm_set_user_var WEZTERM_PROG ""
